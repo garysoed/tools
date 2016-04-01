fbb19f515b0ab428f397779c682a1fb5ddf7b72a
Gary Soedarsono <garysoed@gmail.com>
2016-03-31 22:21:53 -0700

    Finished documenting collections

--- src/collection/maps.ts
+++ src/collection/maps.ts
@@ -18,7 +18,7 @@ export class FluentMap<K, V> extends BaseFluent<Map<K, V>> {
   /**
    * Calls the given function for every entry in the map.
    *
-   * @param fn The function to call.This accepts two arguments:
+   * @param fn The function to call. This accepts two arguments:
    *
    * 1.  Value of the entry.
    * 1.  Key of the entry.
@@ -35,7 +35,7 @@ export class FluentMap<K, V> extends BaseFluent<Map<K, V>> {
 /**
  * Collection of methods to help manipulate maps.
  *
- * The general flow is to input an map, do a bunch of transformations, and output the transformed
+ * The general flow is to input a map, do a bunch of transformations, and output the transformed
  * map at the end. Note that the input map should never be used again. This class does not make any
  * guarantee that it will / will not modify the input map.
  *
--- src/collection/sets.ts
+++ src/collection/sets.ts
@@ -1,16 +1,56 @@
+/// @doc
 import BaseFluent from './base-fluent';
 
+/**
+ * Chainable object to manipulate a set.
+ *
+ * @param <T> The type of element in the set.
+ */
 export class FluentSet<T> extends BaseFluent<Set<T>> {
+  /**
+   * @param data The underlying set to modify.
+   */
   constructor(data: Set<T>) {
     super(data);
   }
 };
 
+/**
+ * Collection of methods to help manipulate sets.
+ *
+ * The general flow is to input a set, do a bunch of transformations, and output the transformed
+ * set at the end. Note that the input set should never be used again. This class does not make any
+ * guarantee that it will / will not modify the input set.
+ *
+ * Example:
+ *
+ * ```javascript
+ * Set
+ *     .fromArray([1, 2, 3])
+ *     .data;  // Set([1, 2, 3])
+ * ```
+ *
+ * Note that every value in the set must be of the same type.
+ */
 const Sets = {
+  /**
+   * Starts by using an array object.
+   *
+   * @param <T> Type of the array element.
+   * @param array Array object to start from.
+   * @return Set wrapper object to do operations on.
+   */
   fromArray<T>(array: T[]): FluentSet<T> {
-    return Sets.of(new Set(array));
+    return Sets.of<T>(new Set<T>(array));
   },
 
+  /**
+   * Starts by using a set.
+   *
+   * @param <T> Type of the set element.
+   * @param data The set object to start with.
+   * @return Set wrapper object to do operations on.
+   */
   of<T>(set: Set<T>): FluentSet<T> {
     return new FluentSet(set);
   },
