8aa10518e40e2ab9208a4ec20c86c11cc0f8dd06
Gary Soedarsono <garysoed@gmail.com>
2017-03-29 14:31:21 -0700

    improved hash for objects

--- src/util/hash.ts
+++ src/util/hash.ts
@@ -1,11 +1,20 @@
 export const HASHED_VALUES: Map<any, number> = new Map();
+export const HASHED_OBJECTS: WeakMap<Object, number> = new WeakMap();
 export const GLOBALS = {lastHash: 0};
 
 
+type MapLike<K, V> = {
+  has(key: K): boolean;
+  get(key: K): V | undefined;
+  set(key: K, value: V): void;
+};
+
+export function hash(object: Object): string;
 export function hash(object: any): string {
-  if (!HASHED_VALUES.has(object)) {
-    HASHED_VALUES.set(object, GLOBALS.lastHash);
+  const mapLike: MapLike<any, number> = object instanceof Object ? HASHED_OBJECTS : HASHED_VALUES;
+  if (!mapLike.has(object)) {
+    mapLike.set(object, GLOBALS.lastHash);
     GLOBALS.lastHash++;
   }
-  return `${HASHED_VALUES.get(object)}`;
+  return `${mapLike.get(object)}`;
 }
--- src/util/hash_test.ts
+++ src/util/hash_test.ts
@@ -2,7 +2,7 @@ import { assert, TestBase } from '../test-base';
 TestBase.setup();
 
 import { Mocks } from '../mock/mocks';
-import { GLOBALS, hash, HASHED_VALUES } from '../util/hash';
+import { GLOBALS, hash, HASHED_OBJECTS, HASHED_VALUES } from '../util/hash';
 
 
 describe('util.hash', () => {
@@ -11,20 +11,37 @@ describe('util.hash', () => {
     HASHED_VALUES.clear();
   });
 
-  it('should return the correct hash', () => {
+  it('should return the correct hash for objects ', () => {
     const nextValue = 123;
     GLOBALS.lastHash = nextValue;
     const object = Mocks.object('object');
     assert(hash(object)).to.equal(`${nextValue}`);
-    assert(HASHED_VALUES).to.haveEntries([[object, nextValue]]);
+    assert(HASHED_OBJECTS.get(object)).to.equal(nextValue);
+    assert(GLOBALS.lastHash).to.equal(nextValue + 1);
+  });
+
+  it('should reuse hash value if the object already exists', () => {
+    const hashValue = 123;
+    const object = Mocks.object('object');
+    HASHED_OBJECTS.set(object, hashValue);
+    assert(hash(object)).to.equal(`${hashValue}`);
+    assert(HASHED_OBJECTS.get(object)).to.equal(hashValue);
+  });
+
+  it('should return the correct hash', () => {
+    const nextValue = 123;
+    GLOBALS.lastHash = nextValue;
+    const key = 123;
+    assert(hash(key)).to.equal(`${nextValue}`);
+    assert(HASHED_VALUES).to.haveEntries([[key, nextValue]]);
     assert(GLOBALS.lastHash).to.equal(nextValue + 1);
   });
 
   it('should reuse hash value if it already exists', () => {
     const hashValue = 123;
-    const object = Mocks.object('object');
-    HASHED_VALUES.set(object, hashValue);
-    assert(hash(object)).to.equal(`${hashValue}`);
-    assert(HASHED_VALUES).to.haveEntries([[object, hashValue]]);
+    const key = 123;
+    HASHED_VALUES.set(key, hashValue);
+    assert(hash(key)).to.equal(`${hashValue}`);
+    assert(HASHED_VALUES).to.haveEntries([[key, hashValue]]);
   });
 });
