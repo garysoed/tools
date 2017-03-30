39f568777811c63927b24d244fb281c528cad94c
Gary Soedarsono <garysoed@gmail.com>
2017-03-29 21:45:39 -0700

    lint

--- src/color/colors_test.ts
+++ src/color/colors_test.ts
@@ -2,12 +2,12 @@ import { assert, TestBase } from '../test-base';
 TestBase.setup();
 
 import { Colors } from '../color/colors';
-import { RgbColor } from "src/color/rgb-color";
+import { RgbColor } from '../color/rgb-color';
 
 
 describe('namespace.Colors ', () => {
   describe('getContrast', () => {
-    fit('should return the correct contrast ratio', () => {
+    it('should return the correct contrast ratio', () => {
       const foreground = RgbColor.newInstance(0x12, 0x34, 0x56);
       const background = RgbColor.newInstance(0x78, 0x9a, 0xbc);
       assert(Colors.getContrast(foreground, background)).to.beCloseTo(4.3, 0.01);
@@ -15,7 +15,7 @@ describe('namespace.Colors ', () => {
   });
 
   describe('mix', () => {
-    fit('should return the correct color', () => {
+    it('should return the correct color', () => {
       const color1 = RgbColor.newInstance(0x12, 0x34, 0x56);
       const color2 = RgbColor.newInstance(0x78, 0x9a, 0xbc);
       const mix = Colors.mix(color1, color2, 0.75);
