de738757d6292a627155bfb6b7b04ba732545b16
Gary Soedarsono <garysoed@gmail.com>
2017-04-23 17:44:41 -0700

    Left the hook behind

---
+++ src/webc/hook_test.ts
@@ -0,0 +1,209 @@
+import { assert, Matchers, TestBase } from '../test-base';
+TestBase.setup();
+
+import { Mocks } from '../mock/mocks';
+import { AttributeBinder } from '../webc/attribute-binder';
+import { ChildrenElementsBinder } from '../webc/children-elements-binder';
+import { ClassListBinder } from '../webc/class-list-binder';
+import { ElementSwitchBinder } from '../webc/element-switch-binder';
+import { ANNOTATIONS, Hook } from '../webc/hook';
+import { PropertyBinder } from '../webc/property-binder';
+import { Util } from '../webc/util';
+
+
+describe('webc.hook', () => {
+  const SELECTOR = 'SELECTOR';
+  let hook: Hook;
+
+  beforeEach(() => {
+    hook = new Hook(SELECTOR);
+  });
+
+  describe('createDecorator_', () => {
+    it('should return the correct decorator', () => {
+      const ctor = Mocks.object('ctor');
+      const target = Mocks.object('target');
+      target.constructor = ctor;
+
+      const propertyKey = 'propertyKey';
+      const binder = Mocks.object('binder');
+      const mockBinderFactory = jasmine.createSpy('BinderFactory');
+      mockBinderFactory.and.returnValue(binder);
+
+      const targetEl = Mocks.object('targetEl');
+      spyOn(Util, 'resolveSelector').and.returnValue(targetEl);
+
+      const mockAnnotationHandler =
+          jasmine.createSpyObj('AnnotationHandler', ['attachValueToProperty']);
+      spyOn(ANNOTATIONS, 'forCtor').and.returnValue(mockAnnotationHandler);
+
+      const decorator = hook['createDecorator_'](mockBinderFactory);
+      decorator(target, propertyKey);
+
+      assert(mockAnnotationHandler.attachValueToProperty).to
+          .haveBeenCalledWith(propertyKey, Matchers.any(Function));
+
+      const parentEl = Mocks.object('parentEl');
+      const instance = Mocks.object('instance');
+      assert(mockAnnotationHandler.attachValueToProperty.calls.argsFor(0)[1](parentEl, instance))
+          .to.equal(binder);
+
+      assert(mockBinderFactory).to.haveBeenCalledWith(targetEl, instance);
+      assert(Util.resolveSelector).to.haveBeenCalledWith(SELECTOR, parentEl);
+
+      assert(ANNOTATIONS.forCtor).to.haveBeenCalledWith(ctor);
+    });
+  });
+
+  describe('attribute', () => {
+    it('should attach the correct factory to the property', () => {
+      const attributeName = 'attributeName';
+      const ctor = Mocks.object('ctor');
+      const parser = Mocks.object('parser');
+      const target = Mocks.object('target');
+      target.constructor = ctor;
+
+      const propertyKey = 'propertyKey';
+
+      const mockAnnotationsHandler =
+          jasmine.createSpyObj('AnnotationsHandler', ['attachValueToProperty']);
+      spyOn(ANNOTATIONS, 'forCtor').and.returnValue(mockAnnotationsHandler);
+
+      const targetEl = Mocks.object('targetEl');
+      spyOn(Util, 'resolveSelector').and.returnValue(targetEl);
+
+      const binder = Mocks.object('binder');
+      spyOn(AttributeBinder, 'of').and.returnValue(binder);
+
+      hook.attribute(attributeName, parser)(target, propertyKey);
+      assert(ANNOTATIONS.forCtor).to.haveBeenCalledWith(ctor);
+      assert(mockAnnotationsHandler.attachValueToProperty).to.haveBeenCalledWith(
+          propertyKey,
+          jasmine.any(Function));
+
+      const element = Mocks.object('element');
+      const binderFactory = mockAnnotationsHandler.attachValueToProperty.calls.argsFor(0)[1];
+      assert(binderFactory.call(hook, element)).to.equal(binder);
+      assert(Util.resolveSelector).to.haveBeenCalledWith(SELECTOR, element);
+      assert(AttributeBinder.of).to.haveBeenCalledWith(targetEl, attributeName, parser);
+    });
+  });
+
+  describe('childrenElement', () => {
+    it('should attach the correct factory to the property', () => {
+      const dataHelper = Mocks.object('dataHelper');
+      const ctor = Mocks.object('ctor');
+      const target = Mocks.object('target');
+      const startPadCount = 12;
+      const endPadCount = 34;
+      target.constructor = ctor;
+
+      const propertyKey = 'propertyKey';
+
+      const mockAnnotationsHandler =
+          jasmine.createSpyObj('AnnotationsHandler', ['attachValueToProperty']);
+      spyOn(ANNOTATIONS, 'forCtor').and.returnValue(mockAnnotationsHandler);
+
+      const targetEl = Mocks.object('targetEl');
+      spyOn(Util, 'resolveSelector').and.returnValue(targetEl);
+
+      const binder = Mocks.object('binder');
+      spyOn(ChildrenElementsBinder, 'of').and.returnValue(binder);
+
+      hook.childrenElements(dataHelper, startPadCount, endPadCount)(target, propertyKey);
+      assert(ANNOTATIONS.forCtor).to.haveBeenCalledWith(ctor);
+      assert(mockAnnotationsHandler.attachValueToProperty).to.haveBeenCalledWith(
+          propertyKey,
+          jasmine.any(Function));
+
+      const element = Mocks.object('element');
+      const instance = Mocks.object('instance');
+      assert(mockAnnotationsHandler.attachValueToProperty.calls.argsFor(0)[1](element, instance))
+          .to.equal(binder);
+      assert(Util.resolveSelector).to.haveBeenCalledWith(SELECTOR, element);
+      assert(ChildrenElementsBinder.of).to.haveBeenCalledWith(
+          targetEl,
+          dataHelper,
+          startPadCount,
+          endPadCount,
+          instance);
+    });
+  });
+
+  describe('classList', () => {
+    it('should create the decorator correctly', () => {
+      const binder = Mocks.object('binder');
+      spyOn(ClassListBinder, 'of').and.returnValue(binder);
+
+      const decorator = Mocks.object('decorator');
+      const createDecoratorSpy = spyOn(hook, 'createDecorator_').and.returnValue(decorator);
+
+      assert(hook.classList()).to.equal(decorator);
+      assert(hook['createDecorator_']).to.haveBeenCalledWith(<any> Matchers.any((Function)));
+
+      const element = Mocks.object('element');
+      assert(createDecoratorSpy.calls.argsFor(0)[0](element)).to.equal(binder);
+      assert(ClassListBinder.of).to.haveBeenCalledWith(element);
+    });
+  });
+
+  describe('elementSwitch', () => {
+    it('should create the decorator correctly', () => {
+      const map = new Map();
+      const binder = Mocks.object('binder');
+      spyOn(ElementSwitchBinder, 'of').and.returnValue(binder);
+
+      const decorator = Mocks.object('decorator');
+      const createDecoratorSpy = spyOn(hook, 'createDecorator_').and.returnValue(decorator);
+
+      assert(hook.elementSwitch<number>(map)).to.equal(decorator);
+      assert(hook['createDecorator_']).to.haveBeenCalledWith(<any> Matchers.any((Function)));
+
+      const element = Mocks.object('element');
+      assert(createDecoratorSpy.calls.argsFor(0)[0](element)).to.equal(binder);
+      assert(ElementSwitchBinder.of).to.haveBeenCalledWith(element, map);
+    });
+  });
+
+  describe('innerText', () => {
+    it('should return the correct factory to the property', () => {
+      const decorator = Mocks.object('decorator');
+      spyOn(hook, 'property').and.returnValue(decorator);
+      assert(hook.innerText()).to.equal(decorator);
+      assert(hook.property).to.haveBeenCalledWith('innerText');
+    });
+  });
+
+  describe('property', () => {
+    it('should attach the correct factory to the property', () => {
+      const propertyName = 'propertyName';
+      const ctor = Mocks.object('ctor');
+      const target = Mocks.object('target');
+      target.constructor = ctor;
+
+      const propertyKey = 'propertyKey';
+
+      const mockAnnotationsHandler =
+          jasmine.createSpyObj('AnnotationsHandler', ['attachValueToProperty']);
+      spyOn(ANNOTATIONS, 'forCtor').and.returnValue(mockAnnotationsHandler);
+
+      const targetEl = Mocks.object('targetEl');
+      spyOn(Util, 'resolveSelector').and.returnValue(targetEl);
+
+      const binder = Mocks.object('binder');
+      spyOn(PropertyBinder, 'of').and.returnValue(binder);
+
+      hook.property(propertyName)(target, propertyKey);
+      assert(ANNOTATIONS.forCtor).to.haveBeenCalledWith(ctor);
+      assert(mockAnnotationsHandler.attachValueToProperty).to.haveBeenCalledWith(
+          propertyKey,
+          jasmine.any(Function));
+
+      const element = Mocks.object('element');
+      assert(mockAnnotationsHandler.attachValueToProperty.calls.argsFor(0)[1](element))
+          .to.equal(binder);
+      assert(Util.resolveSelector).to.haveBeenCalledWith(SELECTOR, element);
+      assert(PropertyBinder.of).to.haveBeenCalledWith(targetEl, propertyName);
+    });
+  });
+});
