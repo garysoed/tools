36088b5004348c0f233eff9efd378b6346be2e9b
Gary Soedarsono <garysoed@gmail.com>
2017-05-07 17:59:50 -0700

    Deleted gulp stuff. Fixes #29

--- src/testing/gulpfile.js
+++
@@ -1,6 +0,0 @@
-var gn = require('../../gulp/gulp-node')(__dirname, require('gulp'));
-var typescriptTasks = require('../../gulp-tasks/typescript')(
-    require('gulp-tslint'),
-    require('gulp-typescript'));
-
-gn.exec('lint', typescriptTasks.lint(gn, 'src/event'));
