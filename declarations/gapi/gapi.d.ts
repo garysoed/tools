8538d339a724d9214a26c7951bcdbaeed6021d6a
Gary Soedarsono <garysoed@gmail.com>
2017-10-29 19:36:21 -0700

    declarations for google picker and more to google api

--- declarations/gapi/gapi.d.ts
+++ declarations/gapi/gapi.d.ts
@@ -12,13 +12,22 @@ declare namespace gapi {
     init(config: ClientInitConfig): Promise<void>;
   }
 
+  interface User {
+    getAuthResponse(): gapi.auth2.AuthResponse;
+  }
+
   export function load(libraries: string, callback: () => void): void
 }
 
 declare namespace gapi.auth2 {
   export function getAuthInstance(): Instance
 
+  type AuthResponse = {
+    access_token: string,
+  }
+
   interface Instance {
+    currentUser: {get: () => gapi.User}
     isSignedIn: {get: () => boolean}
     signIn(): Promise<void>
   }
