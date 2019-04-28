64dd3c7fb37507dcfa3853b0c68bdb2dc8ba06a6
Gary Soedarsono <garysoed@gmail.com>
2019-04-28 15:43:58 -0700

    Better output for RXJS debug

--- src/rxjs/debug.ts
+++ src/rxjs/debug.ts
@@ -1,12 +1,33 @@
 import { stringify, Verbosity } from '@moirai';
-import { MonoTypeOperatorFunction } from '@rxjs';
-import { tap } from '@rxjs/operators';
+import { MonoTypeOperatorFunction, Observable } from '@rxjs';
+import { finalize, tap } from '@rxjs/operators';
+
+const SUBSCRIBED_KEYS = new Set<string>();
 
 export function debug<T>(...keys: string[]): MonoTypeOperatorFunction<T> {
-  const key = keys.join('::');
-  return tap(
-      v => console.debug(`[${key}] | ${stringify(v, Verbosity.DEBUG)}`),
-      e => console.debug(`[${key}] ✖ ${stringify(e, Verbosity.DEBUG)}`),
-      () => console.debug(`[${key}] -`),
-  );
+  const baseKey = keys.join('::');
+  return source => new Observable(subscriber => {
+    const key = generateKey(baseKey);
+    SUBSCRIBED_KEYS.add(key);
+    console.debug(`[${key}] ●`);
+    source.pipe(
+        tap(
+            v => console.debug(`[${key}] | ${stringify(v, Verbosity.DEBUG)}`),
+            e => console.debug(`[${key}] ✖ ${stringify(e, Verbosity.DEBUG)}`),
+            () => console.debug(`[${key}] -`),
+        ),
+        finalize(() => SUBSCRIBED_KEYS.delete(key)),
+    ).subscribe(subscriber);
+  });
+}
+
+function generateKey(base: string): string {
+  let i = 0;
+  let attempt = base;
+  while (SUBSCRIBED_KEYS.has(attempt)) {
+    attempt = `${base}::${i}`;
+    i++;
+  }
+
+  return attempt;
 }
