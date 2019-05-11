d4479c93a433f2492db181aae59131c22e49e9ad
Gary Soedarsono <garysoed@gmail.com>
2019-05-11 15:02:44 -0700

    Upgraded webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,48 +2,10 @@ const glob = require("glob");
 const path = require('path');
 const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
 const smp = new SpeedMeasurePlugin();
-const { TsConfigPathsPlugin } = require('awesome-typescript-loader');
+const WebpackBuilder = require('dev/webpack/builder');
 
-module.exports = smp.wrap({
-  entry: glob.sync("./src/**/*.test.ts"),
-  output: {
-    filename: "bundle.js",
-    path: __dirname + "/out"
-  },
-
-  // Enable sourcemaps for debugging webpack's output.
-  devtool: "source-map",
-
-  resolve: {
-    alias: {
-      'rxjs': path.resolve(__dirname, './node_modules/rxjs'),
-    },
-    // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: [".ts", ".tsx", ".js", ".json"],
-    plugins: [
-      new TsConfigPathsPlugin()
-    ]
-  },
-
-  module: {
-    rules: [
-      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
-
-      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-      { test: /\.tsx?$/, loader: "awesome-typescript-loader" }
-    ]
-  },
-
-  mode: "development",
-
-  // When importing a module whose path matches one of the following, just
-  // assume a corresponding global variable exists and use that instead.
-  // This is important because it allows us to avoid bundling all of our
-  // dependencies, which allows browsers to cache those libraries between builds.
-  externals: {
-    "jasmine": "jasmine"
-  },
-
-  watch: true,
-});
+module.exports = (new WebpackBuilder(__dirname))
+    .addEntry('entry', glob.sync('./src/**/*.test.ts'))
+    .setOutput('bundle.js', '/out')
+    .addTypeScript()
+    .buildForDevelopment('gs-tools');
