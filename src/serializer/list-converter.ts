95b13593b7079fbbc76e4c30a7cf01f150be719e
Gary Soedarsono <garysoed@gmail.com>
2019-08-10 23:47:50 -0700

    Fixed list converter to use arrays

--- src/serializer/list-converter.ts
+++ src/serializer/list-converter.ts
@@ -1,11 +1,12 @@
 import { Converter, Serializable } from '@nabu';
-import { createImmutableList, ImmutableList } from '../collect/types/immutable-list';
+
 import { iterableConverter } from './iterable-converter';
 
+
 export function listConverter<T>(itemConverter: Converter<T, Serializable>):
-    Converter<ImmutableList<T>, Serializable> {
+    Converter<T[], Serializable> {
   return iterableConverter(
-      content => createImmutableList([...content]),
+      content => [...content],
       itemConverter,
   );
 }
