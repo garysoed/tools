84aaf5f8d3e1e954f3286c36241c4dad81170017
Gary Soedarsono <garysoed@gmail.com>
2020-05-24 21:27:58 -0700

    Make arrayFrom support file list

--- src/collect/structures/array-from.ts
+++ src/collect/structures/array-from.ts
@@ -1,10 +1,11 @@
 export function arrayFrom(attrs: NamedNodeMap): readonly Attr[];
 export function arrayFrom<N extends Node>(nodeListOf: NodeListOf<N>): readonly N[];
+export function arrayFrom(filelist: FileList): readonly File[];
 export function arrayFrom(nodeListOf: HTMLCollection): readonly Element[];
 export function arrayFrom(
-    collection: NodeListOf<Node>|HTMLCollection|NamedNodeMap,
-): ReadonlyArray<Node|Element> {
-  const array: Array<Node|Element> = [];
+    collection: NodeListOf<Node>|HTMLCollection|NamedNodeMap|FileList,
+): ReadonlyArray<Node|Element|File> {
+  const array: Array<Node|Element|File> = [];
   for (let i = 0; i < collection.length; i++) {
     const item = collection.item(i);
     if (item) {
