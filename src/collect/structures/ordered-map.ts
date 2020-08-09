9ef38f12b4745890b839bd77710e979fcbf264ca
Gary Soedarsono <garysoed@gmail.com>
2020-08-09 14:55:17 -0700

    Added docs to OrderedMap

--- src/collect/structures/ordered-map.ts
+++ src/collect/structures/ordered-map.ts
@@ -11,10 +11,13 @@ import { Sortable } from './sortable';
  * @thModule collect.structures
  */
 export class OrderedMap<K, V> implements ReadonlyMap<K, V>, Sortable<[K, V]> {
+  /**
+   * Default tag when stringifying `OrderedMap`.
+   */
   readonly [Symbol.toStringTag] = 'OrderedMap';
 
-  private readonly _keys = this.source.map(([key]) => key);
-  private readonly _map = new Map(this.source);
+  private readonly keys_ = this.source.map(([key]) => key);
+  private readonly map_ = new Map(this.source);
 
   /**
    * @param source - Array of entry pairs of the map.
@@ -22,11 +25,13 @@ export class OrderedMap<K, V> implements ReadonlyMap<K, V>, Sortable<[K, V]> {
   constructor(private readonly source: ReadonlyArray<readonly [K, V]> = []) { }
 
   /**
+   * Iterates through the entries in the map.
+   *
    * @returns {@link Iterator} for the map.
    */
   [Symbol.iterator](): IterableIterator<[K, V]> {
-    const keys = [...this._keys];
-    const map = new Map(this._map);
+    const keys = [...this.keys_];
+    const map = new Map(this.map_);
     return (function*(): Generator<[K, V]> {
       for (const key of keys) {
         const value = getRequiredValue(map, key);
@@ -36,18 +41,18 @@ export class OrderedMap<K, V> implements ReadonlyMap<K, V>, Sortable<[K, V]> {
   }
 
   /**
-   * @returns Number of elements in the map.
+   * Number of elements in the map.
    */
   get size(): number {
-    return this._keys.length;
+    return this.keys_.length;
   }
 
   /**
    * Removes all entries in the map.
    */
   clear(): void {
-    this._keys.splice(0, this._keys.length);
-    this._map.clear();
+    this.keys_.splice(0, this.keys_.length);
+    this.map_.clear();
   }
 
   /**
@@ -57,21 +62,23 @@ export class OrderedMap<K, V> implements ReadonlyMap<K, V>, Sortable<[K, V]> {
    * @returns True iff deletion was successful.
    */
   delete(key: K): boolean {
-    const index = this._keys.indexOf(key);
+    const index = this.keys_.indexOf(key);
     if (index < 0) {
       return false;
     }
 
-    this._keys.splice(index, 1);
-    this._map.delete(key);
+    this.keys_.splice(index, 1);
+    this.map_.delete(key);
     return true;
   }
 
   /**
+   * Returns all entries in the map.
+   *
    * @returns {@link Iterator} that iterates all entries in the map.
    */
   entries(): IterableIterator<[K, V]> {
-    return this._keys.map(key => [key, getRequiredValue(this._map, key)] as [K, V]).values();
+    return this.keys_.map(key => [key, getRequiredValue(this.map_, key)] as [K, V]).values();
   }
 
   /**
@@ -84,58 +91,74 @@ export class OrderedMap<K, V> implements ReadonlyMap<K, V>, Sortable<[K, V]> {
    * @param callbackfn - Function to call for every entry in the map.
    */
   forEach(callbackfn: (value: V, key: K, map: Map<K, V>) => void): void {
-    for (const key of this._keys) {
-      callbackfn(getRequiredValue(this._map, key), key, this._map);
+    for (const key of this.keys_) {
+      callbackfn(getRequiredValue(this.map_, key), key, this.map_);
     }
   }
 
   /**
+   * Returns value corresponding to the given key.
+   *
+   * @remarks
+   * Returns `undefined` if the value doesn't exist.
+   *
    * @param key - Key of the entry to return.
    * @returns Value corresponding to the given key, or `undefined` if such value does not exist.
    */
   get(key: K): V|undefined {
-    return this._map.get(key);
+    return this.map_.get(key);
   }
 
   /**
+   * Returns entry at the given index.
+   *
+   * @remarks
+   * Returns `undefined` if the value doesn't exist.
+   *
    * @param index - Index of the entry to return.
    * @returns Entry at the given index, or undefined if none exists.
    */
   getAt(index: number): [K, V]|undefined {
-    const key = this._keys[index];
+    const key = this.keys_[index];
     if (key === undefined) {
       return undefined;
     }
 
-    return [key, getRequiredValue(this._map, key)];
+    return [key, getRequiredValue(this.map_, key)];
   }
 
   /**
+   * Returns true iff entry corresponding to the key exists.
+   *
    * @param key - Key to check.
    * @returns True iff an entry corresponding to the given key exists in the map.
    */
   has(key: K): boolean {
-    return this._map.has(key);
+    return this.map_.has(key);
   }
 
   /**
+   * Returns keys in the map.
+   *
    * @returns {@link Iterator} that iterates through the keys in the map.
    */
   keys(): IterableIterator<K> {
-    return this._keys.values();
+    return this.keys_.values();
   }
 
   /**
+   * Sets the entry with the given `key` to the given `value`.
+   *
    * @param key - Key of entry to set.
    * @param value - Value of the entry to set.
    * @returns The current instance of the map with the given key set to the given value.
    */
   set(key: K, value: V): this {
-    if (!this._map.has(key)) {
-      this._keys.push(key);
+    if (!this.map_.has(key)) {
+      this.keys_.push(key);
     }
 
-    this._map.set(key, value);
+    this.map_.set(key, value);
     return this;
   }
 
@@ -145,10 +168,12 @@ export class OrderedMap<K, V> implements ReadonlyMap<K, V>, Sortable<[K, V]> {
    * @param ordering - `Ordering` to sort the map with.
    */
   sort(ordering: Ordering<[K, V]>): void {
-    this._keys.sort(withMap(key => [key, this.get(key)!] as [K, V], ordering));
+    this.keys_.sort(withMap(key => [key, this.get(key)!] as [K, V], ordering));
   }
 
   /**
+   * Modifies the map by deleting and adding entries to the map.
+   *
    * @param start - Index to start modifying the map.
    * @param deleteCount - Number of entries starting at the given start index to delete.
    * @param toAdd - Entries to add at the given start index.
@@ -156,27 +181,29 @@ export class OrderedMap<K, V> implements ReadonlyMap<K, V>, Sortable<[K, V]> {
    */
   splice(start: number, deleteCount: number, ...toAdd: ReadonlyArray<[K, V]>): Array<[K, V]> {
     const addedKeys = toAdd.map(([key]) => key);
-    const deletedKeys = this._keys.splice(start, deleteCount, ...addedKeys);
+    const deletedKeys = this.keys_.splice(start, deleteCount, ...addedKeys);
 
     const deletedPairs: Array<[K, V]> = [];
     for (const deletedKey of deletedKeys) {
-      const value = getRequiredValue(this._map, deletedKey);
+      const value = getRequiredValue(this.map_, deletedKey);
       deletedPairs.push([deletedKey, value]);
-      this._map.delete(deletedKey);
+      this.map_.delete(deletedKey);
     }
 
     for (const [key, value] of toAdd) {
-      this._map.set(key, value);
+      this.map_.set(key, value);
     }
 
     return deletedPairs;
   }
 
   /**
+   * Returns values in the map.
+   *
    * @returns {@link Iterator} that iterates through the values in the map.
    */
   values(): IterableIterator<V> {
-    return this._keys.map(key => getRequiredValue(this._map, key)).values();
+    return this.keys_.map(key => getRequiredValue(this.map_, key)).values();
   }
 }
 
