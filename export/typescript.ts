2237c912a11e8b23876a69d6c4c4b437194f6802
Gary Soedarsono <garysoed@gmail.com>
2021-11-20 14:56:07 -0800

    Added any todo

--- export/typescript.ts
+++ export/typescript.ts
@@ -4,3 +4,4 @@ export {asTuple} from '../src/typescript/as-tuple';
 export {getOwnPropertyKeys} from '../src/typescript/get-own-property-keys';
 export {mapObject} from '../src/typescript/map-object';
 export {Mutable} from '../src/typescript/mutable';
+export {AnyTodo} from '../src/typescript/any-todo';
