b5db6fdb7c34da0aef984008c33d1b5521deff14
Gary Soedarsono <garysoed@gmail.com>
2022-09-25 16:51:29 -0700

    Added condition to break for debugBreak

--- src/rxjs/debug-break.ts
+++ src/rxjs/debug-break.ts
@@ -1,8 +1,13 @@
 import {MonoTypeOperatorFunction} from 'rxjs';
 import {tap} from 'rxjs/operators';
 
-export function debugBreak<T>(): MonoTypeOperatorFunction<T> {
-  return tap(() => {
+type CheckFn<T> = (value: T) => boolean;
+
+export function debugBreak<T>(checkFn: CheckFn<T> = () => true): MonoTypeOperatorFunction<T> {
+  return tap(value => {
+    if (!checkFn(value)) {
+      return;
+    }
     // eslint-disable-next-line no-debugger
     debugger;
   });
